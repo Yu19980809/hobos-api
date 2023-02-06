3119889fcc19345c10c25d7f167e9be002d78ff8
Guangxin Yu <1572250276@qq.com>
2023-02-06 20:25:50 +0800

    added model validation

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,4 +2,8 @@ class Club < ApplicationRecord
   belongs_to :user
   has_many :shows, dependent: :destroy
   has_many :club_followings, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 8, message: '长度为2~8个字符' }
+  validates :address, presence: true, length: { maximum: 16, message: '最长为16个字符' }
+  validates :description, presence: true, length: { maximum: 128, message: '最长为128个字符' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
   has_many :shows, through: :bookings
   has_many :club_followings, dependent: :destroy
   has_many :comedian_followings, dependent: :destroy
+
+  validates :nickname, presence: true, uniqueness: true, length: { minimum: 2, maximum: 8, message: '长度为2~8个字符' }
+  validates :role, presence: true, inclusion: { in: %w(audience comedian manager), message: "%{value} is not a valid role" }
+  validates :slogan, length: { maximum: 16, message: '最长为16个字符' }
+  validates :experience, length: { maximum: 128, message: '最长为128个字符' }
 end
